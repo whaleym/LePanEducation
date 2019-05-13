702490cdff9445c1080e03fb1d68685b10b0b0d4
whaleym <you@example.com>
2019-05-13 23:11:24 +0800

    2019-5-13H

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,10 +4,10 @@ const app = getApp()
 
 Page({
   data: {
-    imgUrls: [
+    swiperImgUrls: [
       {
         index: 0,
-        url: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        url: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640'
       },
       {
         index: 1,
@@ -18,7 +18,37 @@ Page({
         url: 'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
       }
     ],
-    imgSwiperCurrent: 0
+    swiperImgCurrent: 0,
+    itemList: [
+      {
+        index: 0,
+        signupimg: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        img: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        title: '测试课程1',
+        price: '999.00'
+      },
+      {
+        index: 1,
+        signupimg: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        img: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        title: '测试课程2',
+        price: '999.00'
+      },
+      {
+        index: 2,
+        signupimg: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        img: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        title: '测试课程3',
+        price: '999.00'
+      },
+      {
+        index: 3,
+        signupimg: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        img: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        title: '测试课程4',
+        price: '999.00'
+      }],
+      iconsrc: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640'
   },
   //监听页面加载事件
   onLoad: function () {
@@ -27,18 +57,21 @@ Page({
   tapToSearchPage: function() {
     //TODO
     console.log("TODO:跳转到搜索页面");
+    wx.navigateTo({
+      url: '../test/test',
+    })
   },
   //监听轮播图切换事件
   onImgSwiperChange: function(e) {
     //当轮播图切换图片时，标记当前swiper下标
     this.setData({
-      imgSwiperCurrent: e.detail.current
+      swiperImgCurrent: e.detail.current
     });
   },
   //监听轮播图点击事件
   tapImgSwiper: function() {
     //TODO
-    console.log("当前点击的是第" + this.data.imgSwiperCurrent + "张图。");
+    console.log("当前点击的是第" + this.data.swiperImgCurrent + "张图。");
   },
   //监听页面下拉刷新事件
   onPullDownRefresh: function() {
@@ -51,5 +84,15 @@ Page({
       wx.hideLoading();
       wx.stopPullDownRefresh();
     }, 3000);
+  },
+  //查看商品详情
+  tapToDetails: function() {
+    //TODO
+    console.log("查看商品详情");
+  },
+  //商品添加到购物车
+  addToCart: function() {
+    //TODO
+    console.log("加入购物车");
   }
 })
---
+++ pages/test/test.js
@@ -0,0 +1,12 @@
+// pages/test.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    signupimg: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+    imgsrc: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+    iconsrc: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+    list: [1,2,3,4]
+  },
+
+})

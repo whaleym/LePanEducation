f3a1fe3dcf1089e2c6c6b65f068289620a9a1491
whaleym <whaledestine@163.com>
2019-05-17 17:50:58 +0800

    2019-5-17C

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,7 @@
 //index.js
 //获取应用实例
 const app = getApp()
+var isTapCart = false;
 
 Page({
   data: {
@@ -19,7 +20,39 @@ Page({
       }
     ],
     swiperImgCurrent: 0,
-    itemList: [
+    categoryList: [
+      {
+        index: 0,
+        img: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        name: '分类1'
+      },
+      {
+        index: 1,
+        img: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        name: '分类2'
+      },
+      {
+        index: 2,
+        img: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        name: '分类3'
+      },
+      {
+        index: 3,
+        img: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        name: '分类4'
+      },
+      {
+        index: 4,
+        img: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        name: '分类5'
+      },
+      {
+        index: 5,
+        img: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        name: '分类6'
+      },
+    ],
+    goodsList: [
       {
         index: 0,
         signupimg: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
@@ -47,6 +80,20 @@ Page({
         img: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
         title: '测试课程4',
         price: '999.00'
+      },
+      {
+        index: 4,
+        signupimg: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        img: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        title: '测试课程5',
+        price: '999.00'
+      },
+      {
+        index: 5,
+        signupimg: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        img: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+        title: '测试课程6',
+        price: '999.00'
       }],
       iconsrc: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640'
   },
@@ -72,6 +119,10 @@ Page({
   tapImgSwiper: function() {
     //TODO
     console.log("当前点击的是第" + this.data.swiperImgCurrent + "张图。");
+    wx.showToast({
+      title: 'TODO:前往第' + (this.data.swiperImgCurrent + 1) + "页面",
+      icon: 'none'
+    });
   },
   //监听页面下拉刷新事件
   onPullDownRefresh: function() {
@@ -85,14 +136,39 @@ Page({
       wx.stopPullDownRefresh();
     }, 3000);
   },
+  tapToCategory: function(e) {
+    wx.showToast({
+      title: 'TODO:跳转到第' + (e.currentTarget.dataset.num + 1) + '分类',
+      icon: 'none'
+    });
+  },
   //查看商品详情
-  tapToDetails: function() {
+  tapToDetails: function(e) {
     //TODO
-    console.log("查看商品详情");
+    if(!isTapCart) {
+      console.log("查看商品详情");
+      wx.showToast({
+        title: 'TODO:查看商品详情',
+        icon: 'none'
+      });
+    }
   },
   //商品添加到购物车
   addToCart: function() {
     //TODO
     console.log("加入购物车");
+    isTapCart = true;
+    wx.showLoading({
+      title: "loading...",
+      mask: true
+    });
+    setTimeout(function() {
+      wx.hideLoading();
+      wx.showToast({
+        title: 'TODO:打开购物车页面',
+        icon: 'none'
+      });
+      isTapCart = false;
+    }, 500);
   }
 })
